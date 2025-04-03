3023acfa4d3c79a5e353b5902d1d035b41f4a678
sbx_user1051 <sbx_user1051@169.254.6.251>
2025-04-03 14:52:57 +0000

    Thu Apr 03 14:52:57 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,6 +13,10 @@ class CheckoutSolution:
         for a in skus:
             if a in dict:
                 dict[a] += 1
+        price = 0
+        price += dict['C'] * 20 + dict['D'] * 15
+        price += dict['B']//2 * 45 + dict['B'] % 2 * 30
+        price += dict['A']//3 * 130 + dict['A'] % 3 * 50
+
         
-        
-        return 
+        return price
