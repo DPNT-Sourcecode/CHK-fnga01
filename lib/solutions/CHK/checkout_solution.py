89f0c513f0801059626eb6daeb93b1237e2695d6
sbx_user1051 <sbx_user1051@169.254.6.251>
2025-04-03 14:59:24 +0000

    Thu Apr 03 14:59:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,10 +20,11 @@ class CheckoutSolution:
         dict['B'] -= dict['E']//2        
     
         price = 0
-        price += dict['C'] * 20 + dict['D'] * 15 + dict['E'] * 20
+        price += dict['C'] * 20 + dict['D'] * 15 + dict['E'] * 40
         price += dict['B']//2 * 45 + dict['B'] % 2 * 30
         # A: offers for buying 5 and 3, 5 takes priority
         price += dict['A']//5 * 200 + dict['A'] % 5 //3 * 130 + (dict['A'] % 5) % 3 * 50
 
         
         return price
+
