c5782ab31eaea483cfd4a18b6393853e34ef7d54
sbx_user1051 <sbx_user1051@169.254.6.251>
2025-04-03 14:49:57 +0000

    Thu Apr 03 14:49:57 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,16 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        print(0)
-        return 0
+        skus.sort()
+        dict = {
+            'A': 0,
+            'B': 0,
+            'C': 0,     
+            'D': 0,
+        }
+        for a in skus:
+            if a in dict:
+                dict[a] += 1
+        
+        
+        return 
