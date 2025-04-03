ccda2d42b99d0956ee2d3923e9156c4292854ea9
sbx_user1051 <sbx_user1051@169.254.6.251>
2025-04-03 14:56:49 +0000

    Thu Apr 03 14:56:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,14 +9,18 @@ class CheckoutSolution:
             'B': 0,
             'C': 0,     
             'D': 0,
+            'E': 0,
         }
         for a in skus:
             if a in dict:
                 dict[a] += 1
             else:
                 return -1
+            
+                
+    
         price = 0
-        price += dict['C'] * 20 + dict['D'] * 15
+        price += dict['C'] * 20 + dict['D'] * 15 + dict['E'] * 20
         price += dict['B']//2 * 45 + dict['B'] % 2 * 30
         price += dict['A']//3 * 130 + dict['A'] % 3 * 50
 
